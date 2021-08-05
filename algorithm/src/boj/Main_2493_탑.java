1bf5d64349588b1fcd4b809d3dfe711d096160c0
yejiin <yj_song67@naver.com>
2021-08-05 23:52:30 +0900

    0805 algo

---
+++ algorithm/src/boj/Main_2493_탑.java
@@ -0,0 +1,70 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main_2493_탑 {
+
+//	static String str = "6\r\n" + "7 4 9 3 6 8";
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+//		in = new BufferedReader(new StringReader(str));
+
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(in.readLine());
+        int[] top = new int[N + 1];
+
+        StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+        for (int i = 1; i <= N; i++) {
+            top[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // 결과 배열
+        int[] res = new int[N + 1];
+
+        // 스택에 top배열 인덱스 저장
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(1);
+
+        for (int i = 2; i <= N; i++) {
+            int before = stack.pop();
+
+            // 바로 왼쪽 탑의 높이가 자신보다 낮을 때
+            if (top[before] < top[i]) {
+                // 왼쪽에 있는 탑 중 자신보다 높은 탑 찾기
+                while (!stack.isEmpty()) {
+                    int b = stack.pop();
+                    if (top[b] > top[i]) {
+                        res[i] = b;
+                        stack.push(b);
+                        stack.push(i);
+                        break;
+                    }
+                }
+                // 바로 왼쪽 탑의 높이가 자신보다 높을 때
+            } else if (top[before] > top[i]) {
+                res[i] = before;
+                stack.push(before);
+                stack.push(i);
+            }
+
+            // 왼쪽에 있는 탑 중 자신보다 높은 탑이 없을 때
+            if (stack.isEmpty()) {
+                res[i] = 0;
+                stack.push(i);
+            }
+        }
+        for (int i = 1; i <= N; i++) {
+            sb.append(res[i]).append(" ");
+        }
+        System.out.println(sb);
+    }
+
+}
---
+++ algorithm/src/swea/Solution_D4_1218_괄호짝짓기.java
@@ -0,0 +1,85 @@
+package swea;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Stack;
+
+public class Solution_D4_1218_괄호짝짓기 {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+//        System.setIn(new FileInputStream("input.txt"));
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sb = new StringBuilder();
+
+        Character[] left = { '(', '[', '{', '<' };
+
+        for (int tc = 1; tc <= 10; tc++) {
+            sb.append("#").append(tc).append(" ");
+
+            int LEN = Integer.parseInt(in.readLine());
+            boolean result = true;
+
+            Stack<Character> stack = new Stack<>();
+
+            String str = in.readLine();
+            for (int i = 0; i < LEN; i++) {
+                char c = str.charAt(i);
+                if (Arrays.asList(left).contains(c)) {
+                    stack.push(c);
+                } else {
+                    if (!stack.isEmpty()) {
+                        switch (c) {
+                            case ')':
+                                if (stack.peek() == '(') {
+                                    stack.pop();
+                                } else {
+                                    result = false;
+                                }
+                                break;
+                            case ']':
+                                if (stack.peek() == '[') {
+                                    stack.pop();
+                                } else {
+                                    result = false;
+                                }
+                                break;
+                            case '}':
+                                if (stack.peek() == '{') {
+                                    stack.pop();
+                                } else {
+                                    result = false;
+                                }
+                                break;
+                            case '>':
+                                if (stack.peek() == '<') {
+                                    stack.pop();
+                                } else {
+                                    result = false;
+                                }
+                                break;
+                        }
+                    }
+                    if (!result)
+                        break;
+                }
+            }
+            if (!stack.isEmpty()) {
+                result = false;
+            }
+
+            if (result) {
+                sb.append("1\n");
+            } else {
+                sb.append("0\n");
+            }
+
+        }
+        System.out.println(sb);
+    }
+
+}
