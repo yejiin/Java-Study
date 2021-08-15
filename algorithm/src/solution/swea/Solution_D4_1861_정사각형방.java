3d5b82f663caecdf92301b4004e2b55f7bb97f1a
yejiin <yj_song67@naver.com>
2021-08-15 15:46:07 +0900

    8/6

--- algorithm/src/solution/swea/Solution_D4_1861_정사각형방.java
+++ algorithm/src/solution/swea/Solution_D4_1861_정사각형방.java
@@ -8,17 +8,20 @@ import java.util.StringTokenizer;
 
 public class Solution_D4_1861_정사각형방 {
 
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int cntR; // 현재방이 갈 수 있는 방 개수
+
     public static void main(String[] args) throws NumberFormatException, IOException {
-        System.setIn(new FileInputStream("input.txt"));
+//        System.setIn(new FileInputStream("input.txt"));
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         int TC = Integer.parseInt(in.readLine());
-        int[][] room;
 
         for (int tc = 1; tc <= TC; tc++) {
             int N = Integer.parseInt(in.readLine());
-            room = new int[N][N];
+            int[][] room = new int[N][N];
 
             // room 초기화
             for (int i = 0; i < N; i++) {
@@ -28,69 +31,42 @@ public class Solution_D4_1861_정사각형방 {
                 }
             }
 
-            int[] dx = { -1, 1, 0, 0 };
-            int[] dy = { 0, 0, -1, 1 };
+            int maxR = 0; // 최대 이동할 수 있는 방 개수
+            int idxR = Integer.MAX_VALUE; // 이동한 방
 
-            int resCnt = 0; // 최대 이동할 수 있는 방 개수
-            int idx = 0; // 이동한 방
-
-            int cntR = 1; // 현재방이 갈 수 있는 방 개수
             // room 순회하면서 방마다 이동할 수 있는 방으로 이동
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
-                    // 현재 방 숫자
-                    int curN = room[i][j];
-
-                    // 4방향 확인하면서 순회
-                    int x = i;
-                    int y = j;
-                    int nx = 0;
-                    int ny = 0;
-                    int nN = curN;
-                    while (true) {
-                        // 자신보다 1만큼 큰 방 있는지 확인
-                        boolean flag = false;
-
-                        for (int k = 0; k < 4; k++) {
-                            nx = x + dx[k];
-                            ny = y + dy[k];
-                            if (nx < 0 || nx >= N || ny < 0 || ny >= N)
-                                continue;
-
-                            if (room[nx][ny] == nN + 1) {
-                                nN++;
-                                cntR++;
-                                flag = true;
-                                break;
-                            }
-
-                        }
-                        x = nx;
-                        y = ny;
-                        if (!flag) {
-                            break;
-                        }
-
-                    }
-
-                    // 이동할 수 있는 방의 개수가 최대인 방 구하기
-                    if (cntR > resCnt) {
-                        resCnt = cntR;
-                        idx = curN;
-                    } else if (cntR == resCnt) {
-
-                        if (idx > curN) {
-                            idx = curN;
-
-                        }
-                    }
                     cntR = 1;
-                }
+                    getCount(room, i, j, room[i][j]);
 
+                    if (maxR <= cntR) {
+                        if (maxR == cntR) {
+                            idxR = Math.min(room[i][j], idxR);
+                        } else {
+                            idxR = room[i][j];
+                        }
+                        maxR = cntR;
+                    }
+                }
             }
-            sb.append("#").append(tc).append(" ").append(idx).append(" ").append(resCnt).append("\n");
+            sb.append("#").append(tc).append(" ").append(idxR).append(" ").append(maxR).append("\n");
         }
         System.out.println(sb);
     }
 
+    private static void getCount(int[][] room, int curX, int curY, int curNum) {
+
+        int len = room.length;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = curX + dx[i];
+            int ny = curY + dy[i];
+
+            if (nx >= 0 && nx < len && ny >= 0 && ny < len && room[nx][ny] == curNum + 1) {
+                cntR++;
+                getCount(room, nx, ny, curNum + 1);
+            }
+        }
+    }
 }
