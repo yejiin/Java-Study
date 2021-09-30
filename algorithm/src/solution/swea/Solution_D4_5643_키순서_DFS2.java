39b0ff0cd7847a43b1c334e8522424010f8fa1ae
yejiin <yj_song67@naver.com>
2021-10-01 03:02:27 +0900

    algo 키순서 - 다른 방법

---
+++ algorithm/src/solution/swea/Solution_D4_5643_키순서_DFS2.java
@@ -0,0 +1,58 @@
+package solution.swea;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution_D4_5643_키순서_DFS2 {
+
+    static int N, M, adj[][], radj[][];
+    static int cnt;
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int TC = Integer.parseInt(in.readLine());
+        for (int tc = 1; tc <= TC; tc++) {
+            N = Integer.parseInt(in.readLine()); // 학생수
+            M = Integer.parseInt(in.readLine()); // 간선정보수
+
+            adj = new int[N + 1][N + 1];
+            radj = new int[N + 1][N + 1];
+
+            StringTokenizer st = null;
+            for (int m = 0; m < M; m++) {
+                st = new StringTokenizer(in.readLine(), " ");
+                int from = Integer.parseInt(st.nextToken());
+                int to = Integer.parseInt(st.nextToken());
+                adj[from][to] = radj[to][from] = 1;
+            }
+
+            int ans = 0;
+            for (int i = 1; i <= N; i++) {
+                cnt = 0;
+                dfs(i, adj, new boolean[N + 1]); // 자신보다 큰 학생 탐색
+                dfs(i, radj, new boolean[N + 1]); // 자신보다 작은 학생 탐색
+
+                if (cnt == N - 1)
+                    ++ans;
+            }
+            System.out.println("#" + tc + " " + ans);
+        }
+    }
+
+    // 자신보다 큰/작은 학생따라 탐색
+    private static void dfs(int cur, int[][] adj, boolean[] visited) {
+
+        visited[cur] = true;
+
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i] && adj[cur][i] == 1) {
+                ++cnt;
+                dfs(i, adj, visited);
+            }
+        }
+    }
+
+}
