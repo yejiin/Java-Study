30fb85a548249fe049318178f56c3a42c6ff6e5a
yejiin <yj_song67@naver.com>
2022-01-29 00:06:28 +0900

    숫자의 표현

---
+++ algorithm/src/solution/programmars/Lv2/Solution_Lv2_숫자의표현.java
@@ -0,0 +1,21 @@
+package solution.programmars.Lv2;
+
+public class Solution_Lv2_숫자의표현 {
+
+    public int solution(int n) {
+        int answer = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int sum = 0;
+            for (int j = i; j <= n; j++) {
+                sum += j;
+
+                if (sum == n)
+                    answer++;
+                else if (sum > n)
+                    break;
+            }
+        }
+        return answer;
+    }
+}
