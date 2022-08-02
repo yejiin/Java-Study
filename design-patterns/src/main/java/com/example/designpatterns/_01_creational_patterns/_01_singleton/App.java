fcfb985f10fe83b1abca446926449baef8c42817
yejiin <yj_song67@naver.com>
2022-08-02 16:23:56 +0900

    [Design Patterns] 싱글톤 패턴 - 싱글톤 패턴 구현을 깨트리는 방법

--- design-patterns/src/main/java/com/example/designpatterns/_01_creational_patterns/_01_singleton/App.java
+++ design-patterns/src/main/java/com/example/designpatterns/_01_creational_patterns/_01_singleton/App.java
@@ -6,9 +6,38 @@ import java.lang.reflect.InvocationTargetException;
 
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, IOException, ClassNotFoundException {
         Settings1 settings = Settings1.getInstance();
         System.out.println(settings == Settings1.getInstance());
 
+
+        Settings5 settings5 = Settings5.getInstance();
+
+        /**
+         * 싱글톤 패턴 구현을 깨트리는 방법 1
+         * 리플렉션 사용하기 (새로운 인스턴스를 만든 것과 비슷)
+         * 대응 X
+         */
+        Constructor<Settings5> constructor = Settings5.class.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        Settings5 settings51 = constructor.newInstance();
+
+        System.out.println(settings5 == settings51);
+
+        /**
+         * 싱글톤 패턴 구현을 깨트리는 방법 2
+         * 직렬화 & 역직렬화 사용하환
+         * 대응 - readResolve()
+         */
+        Settings5 settings52 = null;
+        try (ObjectOutput out = new ObjectOutputStream(new FileOutputStream("settings.obj"))) {
+            out.writeObject(settings5);
+        }
+
+        try (ObjectInput in = new ObjectInputStream(new FileInputStream("settings.obj"))) {
+            settings52 = (Settings5) in.readObject();
+        }
+
+        System.out.println(settings5 == settings52);
     }
 }
--- design-patterns/src/main/java/com/example/designpatterns/_01_creational_patterns/_01_singleton/Settings5.java
+++ design-patterns/src/main/java/com/example/designpatterns/_01_creational_patterns/_01_singleton/Settings5.java
@@ -6,7 +6,7 @@ import java.io.Serializable;
  * 멀티 쓰레드 환경에서 안전하게 구현하는 방법 4 - 권장하는 방법 중 하나
  * static inner 클래스 사용하기
 */
-public class Settings5 {
+public class Settings5 implements Serializable {
 
     private Settings5() {}
 
@@ -18,4 +18,11 @@ public class Settings5 {
         return SettingsHolder.INSTANCE;
     }
 
+    /**
+     * 역직렬화 대응 방안
+     * @return 동일한 객체 반환
+     */
+    protected Object readResolve() {
+        return getInstance();
+    }
 }
