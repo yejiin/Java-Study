8ee0170b87ad6569ef26287e7daee729b42b35d0
yejiin <yj_song67@naver.com>
2022-08-24 22:48:46 +0900

    [Design Patterns] 브릿지 패턴 - 적용 전 코드

--- design-patterns/src/main/java/com/example/designpatterns/_02_structural_patterns/_07_bridge/App.java
+++ design-patterns/src/main/java/com/example/designpatterns/_02_structural_patterns/_07_bridge/App.java
@@ -6,5 +6,14 @@ public class App {
         Champion kda아리 = new KDA아리();
         kda아리.skillQ();
         kda아리.skillR();
+
+        // bridge pattern
+        Champion kda아리2 = new 아리(new KDA());
+        kda아리2.skillQ();
+        kda아리2.skillR();
+
+        Champion poolParty아리 = new 아리(new PoolParty());
+        poolParty아리.skillQ();
+        poolParty아리.skillR();
     }
 }
