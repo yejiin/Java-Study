86a6a6aaad6980d09e9aae1e685dbbd6eabe34f0
yejiin <yj_song67@naver.com>
2022-08-26 18:08:50 +0900

    [Design Patterns] 브릿지 패턴 - 자바, 스프링에서의 브릿지 패턴

---
+++ design-patterns/src/main/java/com/example/designpatterns/_02_structural_patterns/_07_bridge/java/BrideInSpring.java
@@ -0,0 +1,15 @@
+package com.example.designpatterns._02_structural_patterns._07_bridge.java;
+
+import org.springframework.jdbc.support.JdbcTransactionManager;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.transaction.PlatformTransactionManager;
+
+public class BrideInSpring {
+
+    public static void main(String[] args) {
+        MailSender mailSender = new JavaMailSenderImpl();
+
+        PlatformTransactionManager platformTransactionManager = new JdbcTransactionManager();
+    }
+}
