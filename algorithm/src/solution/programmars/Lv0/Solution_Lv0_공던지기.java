5c0e75c9ebb631b061f777878c0f97b24540f606
yejiin <yjs0672@gmail.com>
2023-01-08 13:45:39 +0900

    Solved Lv0 공 던지기 - programmars

---
+++ algorithm/src/solution/programmars/Lv0/Solution_Lv0_공던지기.java
@@ -0,0 +1,9 @@
+package solution.programmars.Lv0;
+
+public class Solution_Lv0_공던지기 {
+
+    public int solution(int[] numbers, int k) {
+        int index = 2 * (k - 1);
+        return numbers[index % numbers.length];
+    }
+}
