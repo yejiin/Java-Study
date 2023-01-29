5564e7de9c95e785a7ecc479868cc5db620bae68
yejiin <yjs0672@gmail.com>
2023-01-29 22:41:46 +0900

    [Design Patterns] 컴포지트 패턴

---
+++ design-patterns/src/main/java/com/example/designpatterns/_02_structural_patterns/_08_composite/example/Waitress.java
@@ -0,0 +1,15 @@
+package com.example.designpatterns._02_structural_patterns._08_composite.example;
+
+import java.util.Iterator;
+
+public class Waitress {
+    MenuComponent allMenus;
+
+    public Waitress(MenuComponent allMenus) {
+        this.allMenus = allMenus;
+    }
+
+    public void printMenu() {
+        allMenus.print();
+    }
+}
